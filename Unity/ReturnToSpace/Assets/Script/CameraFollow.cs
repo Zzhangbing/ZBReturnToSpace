097ded0b868e273c0f983a6fc4321d7b782554a0
张兵 <45926406+Zzhangbing@users.noreply.github.com>
2018-12-19 11:06:19 +0800

    角色移动

---
+++ Unity/ReturnToSpace/Assets/Script/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform Player;
+    public Vector3 offset;
+    private void Start()
+    {
+        offset = Player.position - transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, Player.position - offset, Time.deltaTime * 5f);
+        Quaternion rotation = Quaternion.LookRotation(Player.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 3f);
+    }
+
+
+}
---
+++ Unity/ReturnToSpace/Assets/Script/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Player : MonoBehaviour
+{
+
+    public Animator playerAnimatior;
+
+    //目标点的坐标
+    private Vector3 mTargetPos;
+
+    private void Start()
+    {
+
+    }
+    public void Update()
+    {
+        //按下鼠标右键时
+        if(Input.GetMouseButton(1))
+        {
+            //获取屏幕坐标
+            Vector3 mScreenPos = Input.mousePosition;
+            //定义射线
+            Ray mRay = Camera.main.ScreenPointToRay(mScreenPos);
+            RaycastHit mHit;
+            //判断射线是否击中地面
+            if(Physics.Raycast(mRay, out mHit))
+            {
+                //获取目标坐标
+                mTargetPos = mHit.point;
+            }
+            //让主角朝目标坐标并向目标移动
+            transform.LookAt(mTargetPos);
+            PlayerMove(mTargetPos);
+        }   
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            playerAnimatior.SetBool("walkF",false);
+        }
+
+    }
+
+    public void PlayerMove(Vector3 mTargetPos)
+    {
+        playerAnimatior.SetBool("walkF", true);
+
+        transform.Translate(Vector3.forward * 2f*Time.deltaTime);
+    }    
+    
+    public void PlayerTurn( Vector3 mTargetPos)
+    {
+        ///右转
+        ///if(newM.z>oldM.z)
+
+    }
+}
